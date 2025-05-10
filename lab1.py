09ac2879a9d6fe8757c159d03dfd8b9a3b032dea
zzy <926466821@qq.com>
2025-05-10 13:46:56 +0800

    修改了代码

--- lab1.py
+++ lab1.py
@@ -8,6 +8,7 @@ import matplotlib as mpl
 import msvcrt  # 用于检测键盘输入
 import math
 
+
 # 设置中文字体
 plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
 plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
